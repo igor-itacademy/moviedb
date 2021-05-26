903ffcf628c9a1a4ebe928e7f010e54f6f319e3a
Igor <8luxfshdnlz1@mail.ru>
2021-05-26 19:16:32 +0600

    Added test for some urls

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import list_movies
+from django.contrib.auth.views import PasswordResetView
 
-# Create your tests here.
+
+class URLTests(TestCase):
+	def test_list_movies(self):
+		url = reverse('list_movies')
+		# self.assertEqual(resolve(url).func.__name__, Class_based_view.as_view().__name__ )
+		# self.assertEqual(resolve(url).func.view_class, Class_based_view )
+		self.assertEqual(resolve(url).func, list_movies )
+
+
+	def test_login_page(self):
+		pass
+
+
+	def test_password_recovery_page(self):
+		url = reverse('password_reset')
+		self.assertEqual(resolve(url).func.view_class, PasswordResetView )
+
