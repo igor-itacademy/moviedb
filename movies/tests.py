f23d92112133ec9905981f18fe10e9777b42b48b
Igor <8luxfshdnlz1@mail.ru>
2021-05-28 19:10:34 +0600

    Added some tests for urls and views

--- movies/tests.py
+++ movies/tests.py
@@ -1,7 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse, resolve
-from .views import list_movies
 from django.contrib.auth.views import PasswordResetView
+from unittest import skip
+from .views import list_movies, activation_page
+
 
 
 class URLTests(TestCase):
@@ -11,7 +13,7 @@ class URLTests(TestCase):
 		# self.assertEqual(resolve(url).func.view_class, Class_based_view )
 		self.assertEqual(resolve(url).func, list_movies )
 
-
+	@skip
 	def test_login_page(self):
 		pass
 
@@ -20,3 +22,16 @@ class URLTests(TestCase):
 		url = reverse('password_reset')
 		self.assertEqual(resolve(url).func.view_class, PasswordResetView )
 
+	def test_activation_page(self):
+		url = reverse('activation_page', args=['uid', 'token'])
+		self.assertEqual(resolve(url).func, activation_page)
+
+
+class TestViews(TestCase):
+	def test_users_list(self):
+		client = Client()
+		url = reverse('users_list')
+		response = client.get(url)
+
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'users/user_list.html')
